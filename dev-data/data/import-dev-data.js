913d88fec44006112b41b1e4ec5b4f8bd5e00f46
Gerard <gerardodgalicia@gmail.com>
2021-12-08 13:10:52 -0300

    Added users/reviews seeder

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const config = require('./../../config');
 const mongoose = require('mongoose');
 const Tour = require('./../../models/tourModel');
+const Review = require('./../../models/reviewModel');
+const User = require('./../../models/userModel');
 
 const DB = config.DATABASE;
 
@@ -9,11 +11,15 @@ mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 // READING THE JSON FILE
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
 
 // IMPORT DATA INTO DB
 const importData = async () => {
 	try {
 		await Tour.create(tours);
+		await User.create(users, { validateBeforeSave: false });
+		await Review.create(reviews);
 		console.log('Data successfully loaded!');
 	} catch(err) {
 		console.log(err);
@@ -25,6 +31,8 @@ const importData = async () => {
 const deleteData = async () => {
 	try {
 		await Tour.deleteMany();
+		await User.deleteMany();
+		await Review.deleteMany();
 		console.log('Data successfully deleted!');
 	} catch(err) {
 		console.log(err);
