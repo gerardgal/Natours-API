446f9221b5562701291c0192663df61afb2485d0
Gerard <gerardodgalicia@gmail.com>
2021-12-11 13:50:51 -0300

    Geospatial queries/aggregation

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -22,6 +22,14 @@ router
 	.get(authController.protect, authController.restrictTo('admin', 'lead-guide', 'guide'), tourController.getMonthlyPlan);
 
 router
+	.route('/tours-within/:distance/center/:latlng/unit/:unit')
+	.get(tourController.getToursWithin);
+
+router
+	.route('/distances/:latlng/unit/:unit')
+	.get(tourController.getDistances);
+
+router
 	.route('/')
 	.get(tourController.getAllTours)
 	.post(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.createTour);
