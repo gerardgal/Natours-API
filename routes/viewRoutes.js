bb7d5071932d545e83085573b68083b6269d9f73
Gerard <gerardodgalicia@gmail.com>
2022-01-15 16:04:19 -0300

    Cookie MW

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
+
+router.get('/signup', viewsController.getSignUpForm);
+
+router.use(authController.isLoggedIn);
 
 router.get('/', viewsController.getOverview);
 router.get('/tour/:slug', viewsController.getTour);
 router.get('/login', viewsController.getLoginForm);
-router.get('/signup', viewsController.getSignUpForm);
 
 module.exports = router;
