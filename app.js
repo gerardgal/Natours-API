4ba235da0f152175d9f03c0039bf5bcb7f067328
Gerard <gerardodgalicia@gmail.com>
2022-01-15 15:11:49 -0300

    Added cookie-parser

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -44,6 +45,7 @@ app.use('/api', limiter);
 // Body parser
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
 
 // Data sanitization
 app.use(mongoSanitize());
