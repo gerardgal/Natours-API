ce645e61c4d4a669d00f0850897c42965b7a04ca
Gerard <gerardodgalicia@gmail.com>
2022-01-26 16:44:59 -0300

    Updating user data page

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,14 @@ import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
 import { signup } from './signUp';
+import { updateSettings } from './updateSettings';
 
 const mapBox = document.getElementById('map');
-const loginForm = document.querySelector('.login-form');
-const signUpForm = document.querySelector('.signUp-form');
+const loginForm = document.querySelector('.form--login');
+const signUpForm = document.querySelector('.form--signup');
 const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
 if (mapBox) {
   const locations = JSON.parse(mapBox.dataset.locations);
@@ -36,3 +39,29 @@ if (loginForm) {
 }
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
+
+if (userDataForm) {
+  userDataForm.addEventListener('submit', event => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateSettings({ name, email }, 'data');
+  });
+};
+
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit', async event => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateSettings({ passwordCurrent, password, passwordConfirm }, 'password');
+
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+  });
+};
